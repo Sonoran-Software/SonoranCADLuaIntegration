cceb351440a44929dadf2c48132323816a9c422b
Dawson Godin <33760026+DevDawson@users.noreply.github.com>
2020-03-22 23:58:36 -0700

    Version Bump

--- sv_update_check.lua
+++ sv_update_check.lua
@@ -21,7 +21,7 @@ along with this program in the file "LICENSE".  If not, see <http://www.gnu.org/
 -- Update Checker
 ---------------------------------------------------------------------------
 local url = "https://raw.githubusercontent.com/Sonoran-Software/SonoranCADLuaIntegration/master/version.json"
-local version = "1.1.3"
+local version = "1.1.4"
 local latest = true
 
 local rawData = LoadResourceFile(GetCurrentResourceName(), "version.json")
