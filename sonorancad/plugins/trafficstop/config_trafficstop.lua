8ed26b1dea9ce6fdf82ff73be70db9e74d3b44ca
Taylor McGaw <taylor.mcgaw@outlook.com>
2020-05-12 10:51:08 -0400

    [plugin] trafficstop: fixed config

--- sonorancad/plugins/trafficstop/config_trafficstop.lua
+++ sonorancad/plugins/trafficstop/config_trafficstop.lua
@@ -12,11 +12,11 @@ config = {
     requiresPlugins = {"locations"}, -- required plugins for this plugin to work, separated by commas
 
     -- put your configuration options below
-    enablets = true
-    origin = 3 -- 1 = CALLER / 2 = RADIO DISPATCH / 3 = OBSERVED / 4 = WALK_UP
-    status = 2 -- 1 = PENDING / 2 = ACTIVE / 3 = CLOSED
-    priority = 1 -- 1, 2, or 3
-    title = "Traffic Stop" -- This is the title of the call by default it is sent as "Traffic Stop"
+    enablets = true,
+    origin = 3, -- 1 = CALLER / 2 = RADIO DISPATCH / 3 = OBSERVED / 4 = WALK_UP
+    status = 2, -- 1 = PENDING / 2 = ACTIVE / 3 = CLOSED
+    priority = 1, -- 1, 2, or 3
+    title = "Traffic Stop", -- This is the title of the call by default it is sent as "Traffic Stop"
     code = "10-11 - Traffic Stop" -- Change this to reflect your communities 10 Code for a Traffic Stop
 }
 
