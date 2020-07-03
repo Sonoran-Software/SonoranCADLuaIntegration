332250a38dd8b6a1d684214a8348c38ea0f22d60
Nathan C <me@nathanc.tech>
2020-07-03 19:41:36 -0400

    feature: add log generation for support purposes

--- sonorancad/core/commands.lua
+++ sonorancad/core/commands.lua
@@ -15,6 +15,46 @@
     update - attempt to auto-update
 ]]
 
+local function dumpInfo()
+    local version = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+    local pluginList, loadedPlugins, disabledPlugins = GetPluginLists()
+    local pluginVersions = {}
+    for k, v in pairs(pluginList) do
+        if Config.plugins[v] then
+            table.insert(pluginVersions, ("%s [%s]"):format(v, Config.plugins[v].version))
+        end
+    end
+    return ([[
+SonoranCAD
+Version: %s
+Available Plugins
+%s
+Loaded Plugins
+%s
+Disabled Plugins
+%s
+    ]]):format(version, table.concat(pluginVersions, ", "), table.concat(loadedPlugins, ", "), table.concat(disabledPlugins, ", "))
+end
+
+local function dumpPlugin(name)
+    local pluginDetail = {}
+    if not Config.plugins[name] then
+        print("Bad plugin: "..name)
+        return nil
+    end
+    for k, v in pairs(Config.plugins[name]) do
+        table.insert(pluginDetail, ("%s = %s"):format(k, v))
+    end
+    return ([[
+Plugin: %s
+Version: %s
+Configuration:
+    %s
+    ]]):format(name, Config.plugins[name].version, table.concat(pluginDetail, "\n     "))
+end
+
+registerApiType("UPLOAD_LOGS", "support")
+--"apiUrl": "https://cadapi.dev.sonoransoftware.com/",
 RegisterCommand("sonoran", function(source, args, rawCommand)
     if source ~= 0 then
         print("Console only command")
@@ -24,44 +64,63 @@ RegisterCommand("sonoran", function(source, args, rawCommand)
         print("Missing command. Try \"sonoran help\" for help.")
         return
     end
-    if args[1] == "debugmode" then
+    if args[1] == "help" then
+        print([[
+SonoranCAD Help
+    debugmode - Toggles debugging mode
+    info - dump version info, configuration
+    support - dump useful data for support staff 
+    verify - run hash checks to confirm all files are untampered
+    plugin <name> - show info about a plugin (config)
+    update - attempt to auto-update
+]])
+    elseif args[1] == "debugmode" then
         Config.debugMode = not Config.debugMode
         infoLog(("Debug mode toggled to %s"):format(Config.debugMode))
     elseif args[1] == "info" then
-        local version = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
-        local pluginList, loadedPlugins, disabledPlugins = GetPluginLists()
-        local pluginVersions = {}
-        for k, v in pairs(pluginList) do
-            if Config.plugins[v] then
-                table.insert(pluginVersions, ("%s [%s]"):format(v, Config.plugins[v].version))
-            end
+        print(dumpInfo())
+    elseif args[1] == "support" and args[2] ~= nil then
+        infoLog("Please wait, gathering required data...")
+        local cadOutput = {}
+        cadOutput.key = tonumber(args[2])
+        if cadOutput.key == nil then
+            errorLog("Invalid support key.")
+            return
         end
-        print(([[
-SonoranCAD
-    Version: %s
-Available Plugins
-    %s
-Loaded Plugins
-    %s
-Disabled Plugins
-    %s
-        ]]):format(version, table.concat(pluginVersions, ", "), table.concat(loadedPlugins, ", "), table.concat(disabledPlugins, ", ")))
-    elseif args[1] == "support" then
-        return
+        local plugins = {}
+        for name, config in pairs(Config.plugins) do
+            pluginData = {}
+            pluginData.name = name
+            pluginData.version = config.version
+            pluginData.config = config
+            table.insert(plugins, pluginData)
+        end
+        cadOutput.plugins = plugins
+        cadOutput.logs = ([[
+    SonoranCAD Support Output
+---------------------------------------
+Configuration Information
+---
+%s
+
+---------------------------------------
+Console Buffer
+------
+%s
+        ]]):format(dumpInfo(), GetConsoleBuffer()) --GetConsoleBuffer()
+        Config.debugMode = false
+        performApiRequest({cadOutput}, "UPLOAD_LOGS", function(data)
+            if data == "LOGS UPDATED" then
+                infoLog("Support logs have been successfully uploaded. Debug mode was disabled during the upload.")
+            else
+                errorLog(("Failed to upload support logs: %s"):format(data))
+            end
+        end)
     elseif args[1] == "verify" then
         return
     elseif args[1] == "plugin" and args[2] then
         if Config.plugins[args[2]] then
-            local pluginDetail = {}
-            for k, v in pairs(Config.plugins[args[2]]) do
-                table.insert(pluginDetail, ("%s = %s"):format(k, v))
-            end
-            print(([[
-Plugin: %s
-Version: %s
-Configuration:
-     %s
-            ]]):format(args[2], Config.plugins[args[2]].version, table.concat(pluginDetail, "\n     ")))
+            print(dumpPlugin(args[2]))
         else
             errorLog("Invalid plugin")
         end
