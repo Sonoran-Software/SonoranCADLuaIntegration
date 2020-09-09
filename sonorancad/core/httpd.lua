497d12380b3c0c23aac80ccbc3766458dd95378a
Nathan C <me@nathanc.tech>
2020-09-09 12:38:52 -0400

    Commit httpd.lua (somehow missed???)

---
+++ sonorancad/core/httpd.lua
@@ -0,0 +1,37 @@
+local function getConfig()
+    local config = LoadResourceFile(GetCurrentResourceName(), "config.json")
+    return config
+end
+
+SetHttpHandler(function(req, res)
+    local path = req.path
+    local method = req.method
+
+    if method == 'POST' and path == '/info' then
+        req.setDataHandler(function(body)
+            if not body then
+                res.send(json.encode({["error"] = "bad request"}))
+                return
+            end
+            local data = json.decode(body)
+            if not data then
+                res.send(json.encode({["error"] = "bad request"}))
+                return
+            end
+            if data.password ~= Config.apiKey then
+                res.send(json.encode({["error"] = "bad request"}))
+                return
+            end
+            res.send(json.encode({["status"] = "ok", ["cadInfo"] = string.gsub(dumpInfo(), "\n", "<br />"), ["config"] = string.gsub(getConfig(), "\r\n", "<br />")}))
+
+        end)
+    else
+        if path == '/' then
+            local html = LoadResourceFile(GetCurrentResourceName(), '/core/html/index.html')
+            res.send(html)
+        else
+            res.send("hmm")
+        end
+    end
+
+end)
