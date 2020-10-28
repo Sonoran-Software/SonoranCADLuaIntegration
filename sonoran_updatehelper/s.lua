ea552ca60c4226320897a16004aa12db178af865
Nathan C <me@nathanc.tech>
2020-10-27 21:50:54 -0400

    updater: prevent accidental startup of updatehelper

--- sonoran_updatehelper/s.lua
+++ sonoran_updatehelper/s.lua
@@ -1,6 +1,16 @@
 ManagedResources = { "wk_wars2x", "tablet", "pNotify", "sonoran_livemap", "sonorancad"}
 
 CreateThread(function()
+    file = io.open(GetResourcePath(GetCurrentResourceName()).."/run.lock", "a+")
+    io.input(file)
+    line = io.read()
+    if line ~= "1" then
+        file:close()
+        os.remove(GetResourcePath(GetCurrentResourceName()).."/run.lock")
+        print("sonoran_updatehelper is for internal use and should not be started as a resource.")
+        return
+    end
+    file:close()
     ExecuteCommand("refresh")
     Wait(1000)
     for k, v in pairs(ManagedResources) do
