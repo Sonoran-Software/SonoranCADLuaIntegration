c0fb29a402a914a984b6457cc297e15e5ab3029a
Nathan C <me@nathanc.tech>
2020-11-19 18:13:46 -0500

    fix: remove mysql dependency, don't need it

--- sonorancad/fxmanifest.lua
+++ sonorancad/fxmanifest.lua
@@ -3,7 +3,7 @@ games {'gta5'}
 
 author 'Sonoran CAD'
 description 'Sonoran CAD FiveM Integration'
-version '2.3.0'
+version '2.3.1'
 
 server_scripts {
     'core/http.js'
@@ -20,7 +20,6 @@ server_scripts {
     ,'plugins/**/sv_*.lua'
     ,'plugins/**/sv_*.js'
     ,'core/plugin_loader.lua'
-    ,'@mysql-async/lib/MySQL.lua' -- if not using ESX, you can remove this line
                }
 client_scripts {
     'core/shared_functions.lua'
