586b599ebb40b61cb007a1de39360d6fc68d54b1
Nathan C <me@nathanc.tech>
2021-05-29 22:01:22 -0400

    don't change radar config

---
+++ sonorancad/plugins/vehstat/vehstat/sv_vehstat.lua
@@ -0,0 +1,17 @@
+--[[
+    Sonaran CAD Plugins
+
+    Plugin Name: template
+    Creator: template
+    Description: Describe your plugin here
+
+    Put all server-side logic in this file.
+]]
+
+CreateThread(function() Config.LoadPlugin("pluginname", function(pluginConfig)
+
+    if pluginConfig.enabled then
+
+        -- logic here
+
+end) end)
