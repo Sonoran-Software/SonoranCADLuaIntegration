52d07c7d2bdcf562788bb52f64fdf7b3a10a2e1b
Nathan C <me@nathanc.tech>
2021-06-09 01:54:54 -0400

    bump to 2.6.1, update resolves #42

--- sonorancad/plugins/template/template/CHANGEMEconfig_plugin.lua
+++ sonorancad/plugins/template/template/CHANGEMEconfig_plugin.lua
@@ -9,7 +9,7 @@
 local config = {
     enabled = false,
     configVersion = "1.0",
-    pluginName = "template", -- name your plugin here
+    pluginName = "yourpluginname", -- name your plugin here
     pluginAuthor = "SonoranCAD", -- author
 
     -- put your configuration options below
