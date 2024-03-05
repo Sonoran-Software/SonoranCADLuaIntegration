bfa4c72b20af28963f7dbf11902a5e34f6fc0235
Jordan.#2139 <jordansilva213@gmail.com>
2024-03-05 18:11:37 -0500

    Add universal handler for 911 calls and update version to 2.9.23

--- sonorancad/core/server.lua
+++ sonorancad/core/server.lua
@@ -236,3 +236,26 @@ AddEventHandler("SonoranCAD::core:PlayerReady", function()
         warnLog(("Player %s connected, but did not have an %s ID."):format(source, Config.primaryIdentifier))
     end
 end)
+
+-- Jordan - Add universal handler for 911 calls
+function call911(caller, location, description, postal, plate, cb)
+	exports['sonorancad']:performApiRequest({
+		{
+			['serverId'] = GetConvar('sonoran_serverId', 1),
+			['isEmergency'] = true,
+			['caller'] = caller,
+			['location'] = location,
+			['description'] = description,
+			['metaData'] = {
+				['plate'] = plate,
+				['postal'] = postal
+			}
+		}
+	}, 'CALL_911', cb)
+end
+
+RegisterNetEvent('SonoranScripts::Call911', function(caller, location, description, postal, plate, cb)
+	call911(caller, location, description, postal, plate, function(response)
+		json.encode(response) -- Not, CB's can only be used on the server side, so we just print this here for you to see.
+	end)
+end)
